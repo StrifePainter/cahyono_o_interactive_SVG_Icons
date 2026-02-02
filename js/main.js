fe2565378269aa9c9159650cfbc41f8b17460507
Olivia Cahyono <66972950+StrifePainter@users.noreply.github.com>
2026-02-02 00:57:45 -0500

    Adding Icons

--- js/main.js
+++ js/main.js
@@ -1,41 +1,10 @@
-console.log("JS file is connected");
+console.log("JavaScript file is connected!");
 
-// Variables
+const icon = document.querySelectorAll("svg");
+console.log(icon);
 
-const present = document.querySelector("#Present");
-console.log(present);
-
-const leaf = document.querySelector("#Leaf");
-console.log(leaf);
-
-const banana = document.querySelector("#Banana");
-console.log(banana);
-
-const bag = document.querySelector("#Bag");
-console.log(bag);
-
-const fossil = document.querySelector("#Fossil");
-console.log(fossil);
-
-const flower = document.querySelector("#flower");
-console.log(flower);
-
-// Functions
-
-function logId() {
-    console.log("User Clicked On:", this.id)
+function logId(){
+    console.log(this.id);
 }
 
-// EventListener
-
-present.addEventListener("click", logId);
-
-leaf.addEventListener("click", logId);
-
-banana.addEventListener("click", logId);
-
-bag.addEventListener("click", logId);
-
-fossil.addEventListener("click", logId);
-
-flower.addEventListener("click", logId);
+icon.forEach(icon => icon.addEventListener('click', logId));
